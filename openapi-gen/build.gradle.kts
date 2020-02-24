1bf6f8d927fe8aac88f1fa7473d9791c1c7802a8
zerkshohei <zerkshohei@users.noreply.github.com>
2020-02-25 01:00:31 +0900

    Improve: openapi generate setting

--- openapi-gen/build.gradle.kts
+++ openapi-gen/build.gradle.kts
@@ -3,8 +3,15 @@ plugins {
     id("org.openapi.generator").version("4.2.1")
 }
 
-openApiGenerate {
-
-}
 dependencies {
 }
+
+
+// TODO: 外部変数化, output-pathの変更
+openApiGenerate {
+    generatorName.set("kotlin-spring");
+    inputSpec.set("${projectDir}/spec/openapi.yml")
+}
+
+openApiValidate { inputSpec.set("${projectDir}/spec/openapi.yml") }
+
